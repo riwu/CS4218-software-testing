93c250f772327c1298f732d4bcf8875e8a273662
Dexter Mooi <mooicydexter@gmail.com>
2018-02-07 09:50:30 +0800

    Added test folder structure with empty test suite files

---
+++ test/sg/edu/nus/comp/cs4218/impl/ImplTestSuite.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.comp.cs4218.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ImplTestSuite {
+	
+	@Test
+	public void abc() {
+		assertEquals(1, 1);
+	}
+}

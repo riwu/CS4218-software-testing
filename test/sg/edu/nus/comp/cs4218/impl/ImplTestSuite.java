328e593e8ab224e6eddeef5bb0280784dea4523d
Dexter Mooi <mooicydexter@gmail.com>
2018-02-27 12:21:43 +0800

    Remove dummy implementation

--- test/sg/edu/nus/comp/cs4218/impl/ImplTestSuite.java
+++ test/sg/edu/nus/comp/cs4218/impl/ImplTestSuite.java
@@ -1,14 +1,5 @@
 package sg.edu.nus.comp.cs4218.impl;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 public class ImplTestSuite {
-	
-	@Test
-	public void testInvalidAppMessage() {
-		ShellImpl shellImpl = new ShellImpl();
-		assertEquals("Invalid app.", ShellImpl.EXP_INVALID_APP);
-	}
+
 }

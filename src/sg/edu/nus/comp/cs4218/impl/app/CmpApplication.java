6ceb6d02338987d730084f8c45ada776d6e40f78
Dexter Mooi <mooicydexter@gmail.com>
2018-03-14 10:13:07 +0800

    Refactored utility methods into CmpApplicationUtil and updated app and test files

--- src/sg/edu/nus/comp/cs4218/impl/app/CmpApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/CmpApplication.java
@@ -5,26 +5,18 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import sg.edu.nus.comp.cs4218.app.CmpInterface;
+import sg.edu.nus.comp.cs4218.impl.CmpApplicationUtil;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.CmpException;
 
 public class CmpApplication implements CmpInterface {
-
-	private static final String SIMPLE = "Files differ";
-	private static final String NORMAL_FORMAT = "%1$s %2$s differ: %3$s";
-	private static final String NORMAL_DIFF = "char %1$s, line %2$s%3$s";
-	private static final String LONG_FORMAT = "%1$s %2$s %3$s"; //byte# octal1# octal2#
-	private static final String IS_FORMAT = " is %1$s %2$s";
-	private static final String CHAR_FORMAT = "%1$s %2$s"; //octal# value#
-	private static final String STDIN = "-";
+	
 	private boolean isPrintCharDiff, isPrintSimplify, isPrintOctalDiff;
 	
 	@Override
@@ -33,18 +25,15 @@ public class CmpApplication implements CmpInterface {
 		boolean hasStdin;
 		hasStdin = isPrintCharDiff = isPrintSimplify = isPrintOctalDiff = false;
 		List<String> fileNames = new ArrayList<String> ();
-		if(stdout == null) {
+		if(stdin == null || stdout == null) {
 			throw new CmpException("Null pointer exception");
 		}
 		for(int i = 0; i < args.length; i++) {
-			if(STDIN.equals(args[i])) {
-				if(stdin == null) {
-					throw new CmpException("No file to read from stdin");
-				}
-				else {
-					hasStdin = true;
+			if(CmpApplicationUtil.STDIN.equals(args[i])) {
+				if(!hasStdin) {
 					fileCount++;
 				}
+				hasStdin = true;
 			}
 			else if (args[i].startsWith("-")) {
 				extractOptions(args[i]);
@@ -61,12 +50,9 @@ public class CmpApplication implements CmpInterface {
 		if(fileCount != 2) {
 			throw new CmpException("There must be two files to compare");
 		}
-		if(fileNames.isEmpty()) {
-			throw new CmpException("Cannot compare stdin with stdin");
-		}
 		try {
 			stdout.write(compare(stdin, hasStdin, fileNames));
-			if(stdout == System.out) {
+			if(stdout instanceof ByteArrayOutputStream) {
 				stdout.write(System.lineSeparator().getBytes());
 			}
 		} catch (IOException e) {
@@ -108,18 +94,15 @@ public class CmpApplication implements CmpInterface {
 		String message;
 		byte[] bytesA = Files.readAllBytes(Paths.get(fileNameA));
 		byte[] bytesB = Files.readAllBytes(Paths.get(fileNameB));
-		
 		if(isPrintSimplify) {
-			message = getSimpleString(bytesA, bytesB);
+			message = CmpApplicationUtil.getSimpleString(bytesA, bytesB);
 		}
 		else if(isPrintOctalDiff) {
-			message = getLongFormatString(bytesA, bytesB, isPrintCharDiff);
+			message = CmpApplicationUtil.getLongFormatString(bytesA, bytesB, isPrintCharDiff);
 		}
 		else {
-			message = getNormalFormatString(bytesA, bytesB, isPrintCharDiff, false);
-			if(message.length() > 0) {
-				message = String.format(NORMAL_FORMAT, fileNameA, fileNameB, message);
-			}
+			message = CmpApplicationUtil.getNormalFormatString(fileNameA, fileNameB, 
+						bytesA, bytesB, isPrintCharDiff);
 		}
 		return message;
 	}
@@ -138,84 +121,16 @@ public class CmpApplication implements CmpInterface {
 		baos.flush();
 		byte[] bytesB = baos.toByteArray();
 		if(isPrintSimplify) {
-			message = getSimpleString(bytesA, bytesB);
+			message = CmpApplicationUtil.getSimpleString(bytesA, bytesB);
 		}
 		else if(isPrintOctalDiff) {
-			message = getLongFormatString(bytesA, bytesB, isPrintCharDiff);
+			message = CmpApplicationUtil.getLongFormatString(bytesA, bytesB, isPrintCharDiff);
 		}
 		else {
-			message = getNormalFormatString(bytesA, bytesB, isPrintCharDiff, false);
-			if(message.length() > 0) {
-				message = String.format(NORMAL_FORMAT, fileName, STDIN, message);
-			}
+			message = CmpApplicationUtil.getNormalFormatString(fileName, CmpApplicationUtil.STDIN, 
+						bytesA, bytesB, isPrintCharDiff);
 		}
 		return message;
-	}
-	
-	private String getNormalFormatString(byte[] fileA, byte[] fileB, Boolean isCharDiff, boolean hasStdin) throws Exception {
-		int line = 1;
-		StringBuilder strBuilder = new StringBuilder();
-		String appendString = "";
-		for(int i=0; i < fileA.length && i < fileB.length; i++) {
-			if(fileA[i] != fileB[i]) {
-				if(isCharDiff) {
-					appendString = getIsFormatString(fileA[i], fileB[i]);
-				}
-				strBuilder.append(String.format(NORMAL_DIFF, i+1, line, appendString));
-				break;
-			}
-			if(fileA[i] == fileB[i] && fileA[i] == '\n') {
-				line++;
-			}
-		}
-		return strBuilder.toString();
-	}
-
-	private String getSimpleString(byte[] fileA, byte[] fileB) throws Exception {
-		String message;
-		boolean areEqual = Arrays.equals(fileA, fileB);
-		if(areEqual) {
-			message = "";
-		}
-		else {
-			message = SIMPLE;
-		}
-		return message;
-	}
-	
-	private String getIsFormatString(byte first, byte second) {
-		return String.format(IS_FORMAT, getOctalCharString(first), getOctalCharString(second));
-	}
-	
-	private String getOctalCharString(byte single) {
-		return String.format(CHAR_FORMAT, toOctal(single), Byte.toString(single));
-	}
-	
-	private String getLongFormatString(byte[] fileA, byte[] fileB, boolean isCharDiff) throws Exception {
-		StringBuilder strBuilder = new StringBuilder();
-		String diffA, diffB;
-		for(int i=0; i < fileA.length && i < fileB.length; i++) {
-			if(fileA[i] != fileB[i]) {
-				if(strBuilder.length() > 0) {
-					strBuilder.append(System.lineSeparator());
-				}
-				if(isCharDiff) {
-					diffA = getOctalCharString(fileA[i]);
-					diffB = getOctalCharString(fileB[i]);
-				}
-				else {
-					diffA = toOctal(fileA[i]);
-					diffB = toOctal(fileB[i]);
-				}
-				strBuilder.append(String.format(LONG_FORMAT, i+1, diffA, diffB));
-			}
-		}
-		return strBuilder.toString();
-	}
-
-	private String toOctal(byte single) {
-		BigInteger bigInt = new BigInteger(new byte[] {single});
-		return bigInt.toString(8);
 	}
 
 }
--- test/sg/edu/nus/comp/cs4218/impl/app/CmpApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/CmpApplicationTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,23 +16,21 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import sg.edu.nus.comp.cs4218.exception.CmpException;
+import sg.edu.nus.comp.cs4218.impl.CmpApplicationUtil;
 
 public class CmpApplicationTest {
 	private static CmpApplication cmpApp;
 	private static final Path BASE_PATH = Paths.get(System.getProperty("user.dir"));
 	private static final String TEST_FOLDER = "undertest";
-	private static final String TEXT_A = "This is\tthe text";
-	private static final String TEXT_B = "That is\nthe text";
-	private static final String SIMPLE = "Files differ";
-	private static final String NORMAL_FORMAT = "%1$s %2$s differ: char %3$s, line %4$s%5$s";
-	private static final String LONG_FORMAT = "%1$s %2$s %3$s"; //byte# octal1# octal2#
-	private static final String IS_FORMAT = " is %1$s %2$s";
-	private static final String CHAR_FORMAT = "%1$s %2$s"; //octal# value#
-	private static final String STDIN = "-";
+	private static final String TEXT_A = "This is\tthe texta";
+	private static final String TEXT_B = "That is\nthe textb";
 	static Path testFolder;
 	static Path fileA;
 	static Path fileB;
 	static Path fileC;
+	static byte[] bytesA;
+	static byte[] bytesB;
+	static byte[] bytesC;
 	private FileInputStream stdin;
 
 	@BeforeClass
@@ -56,6 +53,9 @@ public class CmpApplicationTest {
 		outStream = new FileOutputStream(fileC.toFile());
 		outStream.write(TEXT_A.getBytes());
 		outStream.close();
+		bytesA = Files.readAllBytes(fileA);
+		bytesB = Files.readAllBytes(fileB);
+		bytesC = Files.readAllBytes(fileC);
 	}
 	
 	@After
@@ -79,7 +79,8 @@ public class CmpApplicationTest {
 	public void shouldShowFirstDiffWhenNoOptionsFilesOnly() throws Exception {
 		String fileNameA = fileA.toString();
 		String fileNameB = fileB.toString();
-		String expected = String.format(NORMAL_FORMAT, fileNameA, fileNameB, 3, 1, "");
+		String expected = CmpApplicationUtil.getNormalFormatString(fileNameA, fileNameB, 
+							bytesA, bytesB, false);
 		assertEquals(expected, cmpApp.cmpTwoFiles(fileNameA, fileNameB, false, false, false));
 	}
 	
@@ -87,15 +88,16 @@ public class CmpApplicationTest {
 	public void shouldShowSimplifiedWhenSimplifyOnFilesOnly() throws Exception {
 		String fileNameA = fileA.toString();
 		String fileNameB = fileB.toString();
-		assertEquals(SIMPLE, cmpApp.cmpTwoFiles(fileNameA, fileNameB, true, true, true));
+		String expected = CmpApplicationUtil.getSimpleString(bytesA, bytesB);
+		assertEquals(expected, cmpApp.cmpTwoFiles(fileNameA, fileNameB, true, true, true));
 	}
 	
 	@Test
 	public void shouldShowDiffCharWhenDiffCharOnFilesOnly() throws Exception {
 		String fileNameA = fileA.toString();
 		String fileNameB = fileB.toString();
-		String appendString = getIsFormatString(TEXT_A.getBytes()[2], TEXT_B.getBytes()[2]);
-		String expected = String.format(NORMAL_FORMAT, fileNameA, fileNameB, 3, 1, appendString);
+		String expected = CmpApplicationUtil.getNormalFormatString(fileNameA, fileNameB, 
+							bytesA, bytesB, true);
 		assertEquals(expected, cmpApp.cmpTwoFiles(fileNameA, fileNameB, true, false, false));
 	}
 
@@ -103,7 +105,7 @@ public class CmpApplicationTest {
 	public void shouldShowLongFormatWhenLongFormatOnFilesOnly() throws Exception {
 		String fileNameA = fileA.toString();
 		String fileNameB = fileB.toString();
-		String expected = getLongFormatString(TEXT_A, TEXT_B, false);
+		String expected = CmpApplicationUtil.getLongFormatString(bytesA, bytesB, false);
 		assertEquals(expected, cmpApp.cmpTwoFiles(fileNameA, fileNameB, false, false, true));
 	}
 	
@@ -111,7 +113,7 @@ public class CmpApplicationTest {
 	public void shouldShowLongFormatCharDiffWhenLongFormatCharDiffOnFilesOnly() throws Exception {
 		String fileNameA = fileA.toString();
 		String fileNameB = fileB.toString();
-		String expected = getLongFormatString(TEXT_A, TEXT_B, true);
+		String expected = CmpApplicationUtil.getLongFormatString(bytesA, bytesB, true);
 		assertEquals(expected, cmpApp.cmpTwoFiles(fileNameA, fileNameB, true, false, true));
 	}
 	
@@ -127,7 +129,8 @@ public class CmpApplicationTest {
 	public void shouldShowFirstDiffWhenNoOptionsFileStdin() throws Exception {
 		String fileNameA = fileA.toString();
 		stdin = new FileInputStream(fileB.toFile());
-		String expected = String.format(NORMAL_FORMAT, fileNameA, STDIN, 3, 1, "");
+		String expected = CmpApplicationUtil.getNormalFormatString(fileNameA, 
+							CmpApplicationUtil.STDIN, bytesA, bytesB, false);
 		assertEquals(expected, cmpApp.cmpFileAndStdin(fileNameA, stdin, false, false, false));
 	}
 	
@@ -135,15 +138,16 @@ public class CmpApplicationTest {
 	public void shouldShowSimplifiedWhenSimplifyOnFileStdin() throws Exception {
 		String fileNameA = fileA.toString();
 		stdin = new FileInputStream(fileB.toFile());
-		assertEquals(SIMPLE, cmpApp.cmpFileAndStdin(fileNameA, stdin, true, true, true));
+		String expected = CmpApplicationUtil.getSimpleString(bytesA, bytesB);
+		assertEquals(expected, cmpApp.cmpFileAndStdin(fileNameA, stdin, true, true, true));
 	}
 	
 	@Test
 	public void shouldShowDiffCharWhenDiffCharOnFileStdin() throws Exception {
 		String fileNameA = fileA.toString();
 		stdin = new FileInputStream(fileB.toFile());
-		String appendString = getIsFormatString(TEXT_A.getBytes()[2], TEXT_B.getBytes()[2]);
-		String expected = String.format(NORMAL_FORMAT, fileNameA, STDIN, 3, 1, appendString);
+		String expected = CmpApplicationUtil.getNormalFormatString(fileNameA, 
+							CmpApplicationUtil.STDIN, bytesA, bytesB, true);
 		assertEquals(expected, cmpApp.cmpFileAndStdin(fileNameA, stdin, true, false, false));
 	}
 
@@ -151,7 +155,7 @@ public class CmpApplicationTest {
 	public void shouldShowLongFormatWhenLongFormatOnFileStdin() throws Exception {
 		String fileNameA = fileA.toString();
 		stdin = new FileInputStream(fileB.toFile());
-		String expected = getLongFormatString(TEXT_A, TEXT_B, false);
+		String expected = CmpApplicationUtil.getLongFormatString(bytesA, bytesB, false);
 		assertEquals(expected, cmpApp.cmpFileAndStdin(fileNameA, stdin, false, false, true));
 	}
 	
@@ -159,7 +163,7 @@ public class CmpApplicationTest {
 	public void shouldShowLongFormatCharDiffWhenLongFormatCharDiffOnFileStdin() throws Exception {
 		String fileNameA = fileA.toString();
 		stdin = new FileInputStream(fileB.toFile());
-		String expected = getLongFormatString(TEXT_A, TEXT_B, true);
+		String expected = CmpApplicationUtil.getLongFormatString(bytesA, bytesB, true);
 		assertEquals(expected, cmpApp.cmpFileAndStdin(fileNameA, stdin, true, false, true));
 	}
 	
@@ -195,54 +199,14 @@ public class CmpApplicationTest {
 	@Test(expected=CmpException.class)
 	public void shouldThrowExceptionWhenStdinNotExist() throws Exception {
 		String fileNameB = fileB.toString();
-		cmpApp.run(new String[] {fileNameB, STDIN}, null, System.out);
+		cmpApp.run(new String[] {fileNameB, CmpApplicationUtil.STDIN}, null, System.out);
 	}
 	
 	@Test(expected=CmpException.class)
 	public void shouldThrowExceptionWhenStdoutNotExist() throws Exception {
 		String fileNameB = fileB.toString();
 		stdin = new FileInputStream(fileC.toFile());
-		cmpApp.run(new String[] {fileNameB, STDIN}, stdin, null);
+		cmpApp.run(new String[] {fileNameB, CmpApplicationUtil.STDIN}, stdin, null);
 	}
 	
-	//Utility methods
-	
-	private String getIsFormatString(byte first, byte second) {
-		return String.format(IS_FORMAT, getOctalCharString(first), getOctalCharString(second));
-	}
-	
-	private String getOctalCharString(byte single) {
-		return String.format(CHAR_FORMAT, toOctal(single), Byte.toString(single));
-	}
-	
-	private String getLongFormatString(String textA, String textB, boolean isCharDiff) {
-		byte[] bytesA = textA.getBytes();
-		byte[] bytesB = textB.getBytes();
-		StringBuilder strBuilder = new StringBuilder();
-		for(int i=0; i < bytesA.length && i < bytesB.length; i++) {
-			if(bytesA[i] != bytesB[i]) {
-				if(strBuilder.length() > 0) {
-					strBuilder.append(System.lineSeparator());
-				}
-				String diffA = "";
-				String diffB = "";
-				if(isCharDiff) {
-					diffA = getOctalCharString(bytesA[i]);
-					diffB = getOctalCharString(bytesB[i]);
-				}
-				else {
-					diffA = toOctal(bytesA[i]);
-					diffB = toOctal(bytesB[i]);
-				}
-				strBuilder.append(String.format(LONG_FORMAT, i+1, diffA, diffB));
-			}
-		}
-		return strBuilder.toString();
-	}
-
-	private String toOctal(byte single) {
-		BigInteger bigInt = new BigInteger(new byte[] {single});
-		return bigInt.toString(8);
-	}
-
 }

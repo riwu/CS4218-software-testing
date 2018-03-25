07dddf6808653e8925987fb18f165ce681a4c1b5
Dyan Chew <12388397+zavfel@users.noreply.github.com>
2018-03-25 19:05:54 +0800

    cat Sed integration test

--- test/sg/edu/nus/comp/cs4218/integration/CatSedIntegrationTest.java
+++ test/sg/edu/nus/comp/cs4218/integration/CatSedIntegrationTest.java
@@ -6,22 +6,22 @@ import org.junit.Test;
 import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.impl.ShellImpl;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.OutputStream;
 import java.nio.file.Files;
 
 import static org.junit.Assert.assertEquals;
 
-public class SedDiffIntegrationTest {
+public class CatSedIntegrationTest {
     private static ShellImpl shell;
+    private OutputStream stdout;
     private static final String ORIGINAL = "This\tis testtest" + System.lineSeparator() + "the\ttest text test" + System.lineSeparator();
     private static final String REPLACED_SECOND = "This\tis testt" + System.lineSeparator() + "the\ttest text t" + System.lineSeparator();
     private static final String DIR_NAME = "sedTestDir";
     private static final String FILENAME1 = "sedFile1";
-    private static final String FILENAME2 = "sedFile2";
     private File dir;
     private File file1;
-    private File file2;
-    private OutputStream stdout;
 
 
     @Before
@@ -33,43 +33,30 @@ public class SedDiffIntegrationTest {
 
         file1 = new File(currentDir + File.separator + DIR_NAME + File.separator + FILENAME1);
         Files.write(file1.toPath(), ORIGINAL.getBytes());
-        file2 = new File(currentDir + File.separator + DIR_NAME + File.separator + FILENAME2);
+
         stdout = new ByteArrayOutputStream();
     }
 
     @After
     public void tearDown() throws Exception {
         file1.delete();
-        file2.delete();
         dir.delete();
         stdout.close();
     }
 
     @Test
-    public void whenReplaceOriginalExpectDiff() throws Exception {
-        String expected = "< This\tis testtest\n> This\tis test";
-        String argument = "sed s/is test/is / " + file1.toString() + " | diff " + file1.toString() + " -";
+    public void whenCatFileWithReplaceSecondExpectChange() throws Exception {
+        String argument = "cat " + file1.toString() + " | sed s/test/t/2";
         shell.parseAndEvaluate(argument, stdout);
 
-        assertEquals(expected, stdout.toString());
+        assertEquals(REPLACED_SECOND, stdout.toString());
     }
 
     @Test
-    public void whenReplaceOriginalWithqFlagExpectDiff() throws Exception {
-        String expected = "Files " + file1.toString() + " and - differ";
-        String argument = "sed s/is test/is / " + file1.toString() + " | diff -q " + file1.toString() + " -";
+    public void whenCatFileWithNoReplaceExpectNoChange() throws Exception {
+        String argument = "cat " + file1.toString() + " | sed s/test1/t/2";
         shell.parseAndEvaluate(argument, stdout);
 
-        assertEquals(expected, stdout.toString());
-    }
-
-    @Test
-    public void whenReplaceIsSameWithsFlagExpectIdentical() throws Exception {
-        String expected = "Files " + file2.toString() + " and - are identical";
-        Files.write(file2.toPath(), REPLACED_SECOND.getBytes());
-        String argument = "sed s/test/t/2 " + file1.toString() + " | diff -s " + file2.toString() + " -";
-        shell.parseAndEvaluate(argument, stdout);
-
-        assertEquals(expected, stdout.toString());
+        assertEquals(ORIGINAL, stdout.toString());
     }
 }
